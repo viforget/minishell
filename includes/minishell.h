99a502bde7a4cbcacbe4eaa043fedcfa395d40d8
Victor Forget <viforget@student.s19.be>
2021-06-12 11:43:15 +0200

    create echo.c for built_in

---
+++ includes/minishell.h
@@ -0,0 +1,53 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/*
+// index value:
+//	0 : binary
+//	1 : echo
+//	2 : pwd
+//	3 : env
+//	4 : cd
+//	5 : export
+//	6 : unset
+//	7 : bash
+//	8 : exit
+//
+// bin : path to the binary if index == 0
+//
+// av : tab to send to the binary
+//
+// pipe :
+//	0 : no pipe
+//	1 : pipe betwen 2 commands
+//	2 : >  between the command and a file (char *file)
+//	3 : >> between the command and a file (char *file)
+//	
+// guil : <  between the command and a file (char *file_g)
+*/
+
+typedef struct s_command
+{
+	int		index;
+	char	*bin;
+	char	**av;
+	int		pipe;
+	int		guil;
+	char	*file;
+	char	*file_g;
+	struct s_command *next;
+}				t_command;
+
+typedef struct s_var
+{
+	char	*name;
+	char	*content;
+	struct s_var *next;
+}				t_var;
+
+#endif
---
+++ srcs/built_in/echo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+size_t  ft_strlen(const char *str);
+int     ft_strcmp(const char *s1, const char *s2);
+
+int bi_echo(char **av)
+{
+	int		i;
+	char	n;
+
+	n = 0;
+	i = 1;
+	if (!ft_strcmp(av[i], "-n"))
+	{
+		n = 1;
+		i++;
+	}
+	while(av[i])
+	{
+		printf("%-*s", (int)ft_strlen(av[i]) + (av[i + 1] != 0), av[i]);
+		i++;	
+	}
+	if (n == 0)
+		printf("%c", '\n');
+	return (1);
+}
